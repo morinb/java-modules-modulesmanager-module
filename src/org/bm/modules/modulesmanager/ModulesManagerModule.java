1a3357aa2cfe6717af6658d95a875f150f50f6bf
Baptiste MORIN <morinb@server>
2012-11-21 17:24:18 +0100

    initial commit

---
+++ src/org/bm/modules/modulesmanager/ModulesManagerModule.java
@@ -0,0 +1,67 @@
+package org.bm.modules.modulesmanager;
+
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+
+import org.bm.modules.modulesmanager.frame.ModulesManagerModuleFrame;
+import org.bm.modules.shared.IModule;
+import org.bm.modules.shared.ModuleFrame;
+
+public class ModulesManagerModule implements IModule {
+
+   ModulesManagerModuleFrame frame = new ModulesManagerModuleFrame();
+
+   @Override
+   public void attach() {}
+
+   @Override
+   public void deattach() {}
+
+   @Override
+   public int getMenuIndex() {
+      // TODO Auto-generated method stub
+      return IModule.MENU_WINDOWS;
+   }
+
+   @Override
+   public int getMenuItemIndex() {
+      return 0;
+   }
+
+   @Override
+   public String getName() {
+
+      return "Modules Manager";
+   }
+
+   @Override
+   public boolean hasMnemonic() {
+      return true;
+   }
+
+   @Override
+   public int getMnemonic() {
+      // TODO Auto-generated method stub
+      return KeyEvent.VK_M;
+   }
+
+   @Override
+   public boolean hasAccelerator() {
+      // TODO Auto-generated method stub
+      return true;
+   }
+
+   @Override
+   public KeyStroke getAccelerator() {
+      // TODO Auto-generated method stub
+      return KeyStroke.getKeyStroke(KeyEvent.VK_M, InputEvent.ALT_DOWN_MASK);
+   }
+
+   @Override
+   public ModuleFrame getModuleFrame() {
+      return frame;
+   }
+
+}
---
+++ src/org/bm/modules/modulesmanager/frame/ModulesManagerModuleFrame.java
@@ -0,0 +1,13 @@
+package org.bm.modules.modulesmanager.frame;
+
+import org.bm.modules.shared.ModuleFrame;
+
+public class ModulesManagerModuleFrame extends ModuleFrame {
+
+   public ModulesManagerModuleFrame() {
+      this.setSize(800, 600);
+      this.setMaximizable(true);
+      this.setClosable(true);
+
+   }
+}
